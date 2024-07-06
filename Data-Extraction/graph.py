b660237c88ec7242455f817cf789abff31f5fb1e
marciamart <marciamartinsls05@gmail.com>
2024-07-05 23:03:37 -0300

    data extraction

---
+++ Data-Extraction/graph.py
@@ -0,0 +1,26 @@
+class Graph:
+    def __init__(self, file: str):
+        self.file = file
+        self.data = {}
+
+        self.load()
+
+    def load(self):
+        with open(self.file) as f:
+            for line in f:
+                letter, *row = line.split()
+
+                if letter == 'p':
+                    self.v = int(row[1])
+                    self.e = int(row[2])
+                elif letter == 'e':
+                    vertex = int(row[0])
+                    neighbor = int(row[1])
+
+                    if vertex not in self.data:
+                        self.data[vertex] = []
+                    self.data[vertex].append(neighbor)
+                    
+
+        
+        
